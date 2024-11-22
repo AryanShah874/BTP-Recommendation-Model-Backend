4e180a5eaa996a9eb1306aef7c55832abbfbea0c
Aryan Shah <aryanwork10@gmail.com>
2024-11-22 12:25:30 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-// app.use(cors({origin: 'https://lnmiit-faculty-recommendation.vercel.app', credentials: true, methods: 'GET, POST, PATCH, PUT, DELETE'}));
-app.use(cors({origin: 'http://localhost:5173', credentials: true, methods: 'GET, POST, PATCH, PUT, DELETE'}));
+app.use(cors({origin: 'https://lnmiit-faculty-recommendation.vercel.app', credentials: true, methods: 'GET, POST, PATCH, PUT, DELETE'}));
+// app.use(cors({origin: 'http://localhost:5173', credentials: true, methods: 'GET, POST, PATCH, PUT, DELETE'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(express.static('public'));
